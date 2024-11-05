cb1d337c1bda7713ab4b9035c84b2f5e39b3cf95
Priscila Romero <priscillaromero95@gmail.com>
2024-11-05 15:56:05 -0300

    Set recipe and content in state

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,12 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
-import type { Categories, Drink, Drinks, SearchFilter } from "../types"
+import type { Categories, Drink, Drinks, Recipe, SearchFilter } from "../types"
 
 
 export type RecipesSliceType = {
     categories: Categories
     drinks: Drinks
+    selectedRecipe: Recipe
     fetchCategories: () => Promise<void>
     searchRecipes: (searchFilters: SearchFilter) => Promise<void>
     selectRecipe: (id: Drink['idDrink']) => Promise<void>
@@ -18,6 +19,7 @@ export const createRecipesSlace : StateCreator<RecipesSliceType>  = (set) => ({
     drinks: {
         drinks:[]
     },
+    selectedRecipe:{} as Recipe,
     fetchCategories: async () => {
        const categories = await getCategories()
         set({
@@ -32,6 +34,8 @@ export const createRecipesSlace : StateCreator<RecipesSliceType>  = (set) => ({
     },
     selectRecipe: async (id) => {
         const selectedRecipe = await getRecipeById(id)
-        console.log(selectedRecipe)
+        set({
+            selectedRecipe
+        })
     }
 })
