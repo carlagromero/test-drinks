d7eabec3422835e18919c100977713d09f957ccb
Carla Romero <cromero@paciolan.com>
2024-11-28 16:54:09 -0300

    fix: vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // base: 'https://priscila1995.github.io/drinks-and-recipes-app'
-  base: '/drinks-and-recipes-app/'
+  base: ''
 
 })
